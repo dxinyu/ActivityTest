7de04c2818c3a252dd71ed0a7800915518ea2ab7
dxinyu <15901358959@163.com>
2018-12-27 17:11:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -0,0 +1,75 @@
+package com.example.activitytest;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
+public class FirstActivity extends AppCompatActivity {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You click Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You click Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Toast.makeText(FirstActivity.this,"You clicked Button 1",
+                       //Toast.LENGTH_SHORT).show();
+               //finish();
+               //Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+               //Intent intent = new Intent("com.example.activitytest.ACTION_START");
+                //intent.addCategory("com.example.activitytest.MY_CATEGORY");
+               // Intent intent = new Intent(Intent.ACTION_VIEW);
+                //intent.setData(Uri.parse("http://www.baidu.com"));
+
+                //Intent intent = new Intent(Intent.ACTION_DIAL);
+                //intent.setData(Uri.parse("tel:10086"));
+
+                //String data = "Hello SecondActivity";
+                //Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                //intent.putExtra("extra_data", data);
+                //startActivity(intent);
+
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 1);
+            }
+            protected void onActivityResult(int requestCode, int resultCode, Intent data){
+                switch (requestCode){
+                    case 1:
+                        if(resultCode ==RESULT_OK){
+                            String returnedData = data.getStringExtra("data_return");
+                            Log.d("FirstActivity", returnedData);
+                        }
+                        break;
+                        default:
+                }
+            }
+        });
+    }
+}
